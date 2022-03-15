29fb8714a5478bd3a1e9660a4082aba4cac78801
sunpeihan <sunpeihan@mail.ustc.edu.cn>
2022-03-15 11:13:11 +0800

    update

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+import time
+import webbrowser
+
+
+def localtime():
+    res = time.asctime(time.localtime(time.time()))
+    print(res)
+
+
+url = "https://yz.ustc.edu.cn"
+html = requests.get(url)
+html.encoding = html.apparent_encoding
+html_prev = html
+while True:
+    html = requests.get(url)
+    html.encoding = html.apparent_encoding
+    if html.text != html_prev.text:
+        print(f"网页有更新！{localtime()}")
+        webbrowser.open(url)
+        time.sleep(600)
+    else:
+        html_prev = html
+        localtime()
+        time.sleep(60)
